e736fa27d233e1f95b4d16d75ce3c442aa26764d
Andrew Aquino <dawneraq@gmail.com>
2023-10-19 14:17:13 -0700

    output analyzer's points in correct orientation

--- src/components/onion/Onion.PieceAnalyzer.svelte
+++ src/components/onion/Onion.PieceAnalyzer.svelte
@@ -1,14 +1,13 @@
 <script>
-	import { numLayers, rScale } from "$stores/onion";
+	import { numLayers, rScale, yScale } from "$stores/onion";
 
 	$: layerArcs = Array.from({ length: $numLayers }).map((_, i) => {
 		const layerRadius = $rScale(i + 1);
 
-		return (x) => Math.sqrt(layerRadius * layerRadius - x * x);
+		return (x) => $yScale(Math.sqrt(layerRadius * layerRadius - x * x));
 	});
 </script>
 
 {#each layerArcs as getYOnLayerArc}
-	<!-- TODO flip y axis? to make drawing more straightforward -->
 	<circle r="3" cy={getYOnLayerArc(0)} />
 {/each}
--- src/stores/onion.js
+++ src/stores/onion.js
@@ -9,9 +9,15 @@ export const radius = derived(height, ($height) => $height * radiusPercentage);
 
 export const numLayers = readable(10);
 
-export const rScale = derived([numLayers, radius], ([$numLayers, $radius]) => {
-	return scaleLinear().domain([0, $numLayers]).range([0, $radius]);
-});
+// SVG drawing is flipped upside down
+// need to use this function to normalize every linear (y = f(x)) function's output
+export const yScale = derived(height, ($height) =>
+	scaleLinear().domain([0, $height]).range([$height, 0])
+);
+
+export const rScale = derived([numLayers, radius], ([$numLayers, $radius]) =>
+	scaleLinear().domain([0, $numLayers]).range([0, $radius])
+);
 
 export const numCuts = writable(10);
 
