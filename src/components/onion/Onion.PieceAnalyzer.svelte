c45a9944345336d38b82b0e435955cbbd2589a2f
Andrew Aquino <dawneraq@gmail.com>
2023-10-19 15:08:20 -0700

    in analyzer, plot intersections of cuts and layers

--- src/components/onion/Onion.PieceAnalyzer.svelte
+++ src/components/onion/Onion.PieceAnalyzer.svelte
@@ -1,13 +1,27 @@
 <script>
-	import { numLayers, rScale, yScale } from "$stores/onion";
+	import {
+		cutWidthScale,
+		numCuts,
+		numLayers,
+		rScale,
+		yScale
+	} from "$stores/onion";
 
-	$: layerArcs = Array.from({ length: $numLayers }).map((_, i) => {
-		const layerRadius = $rScale(i + 1);
+	$: layerArcs = Array.from({ length: $numLayers + 1 }).map((_, i) => {
+		const layerRadius = $rScale(i);
 
 		return (x) => $yScale(Math.sqrt(layerRadius * layerRadius - x * x));
 	});
 </script>
 
 {#each layerArcs as getYOnLayerArc}
-	<circle r="3" cy={getYOnLayerArc(0)} />
+	{#each { length: $numCuts } as _, i}
+		{@const cutX = $cutWidthScale(i)}
+		{@const cutY = getYOnLayerArc(cutX)}
+
+		<!-- TODO plot a point at (cutX, 0) if cut doesn't intersect at layer arc -->
+		{#if cutY}
+			<circle r="3" cx={cutX} cy={cutY} />
+		{/if}
+	{/each}
 {/each}
