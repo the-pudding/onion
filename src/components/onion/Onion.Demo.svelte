9ecbee6bcec05b230808bdf2a071569685b3605e
Andrew Aquino <dawneraq@gmail.com>
2024-04-17 12:43:08 -0700

    fix: use default import for paper.js

--- src/components/onion/Onion.Demo.svelte
+++ src/components/onion/Onion.Demo.svelte
@@ -2,7 +2,7 @@
 	import { writable } from "svelte/store";
 	import { setContext } from "svelte";
 	import { scaleLinear } from "d3";
-	import { paper } from "paper";
+	import paper from "paper";
 	import Onion from "$utils/onion";
 	import { formatPercentage, polarToCartesian } from "$utils/math";
 	import OnionAxisX from "$components/onion/Onion.AxisX.svelte";
