39688e4c5697f86ce3c06227b06727c376644e02
Andrew Aquino <dawneraq@gmail.com>
2024-06-14 13:14:36 -0700

    feat: add cut lines + depth labels to OnionReport diagrams

--- src/components/onion/Onion.Report.svelte
+++ src/components/onion/Onion.Report.svelte
@@ -1,9 +1,14 @@
 <script>
+	import { scaleLinear } from "d3";
 	import Select from "$components/helpers/Select.svelte";
 	import SortTable from "$components/helpers/SortTable.svelte";
-	import { pluralize } from "$lib/utils/pluralize";
+	import { pluralize } from "$utils/pluralize";
 
 	const diagramSize = 32;
+	const diagramPadding = 4;
+	const halfDiagramSize = diagramSize / 2;
+	const diagramExtent = halfDiagramSize - diagramPadding;
+	const radialTargetPosition = diagramPadding / 2;
 
 	// TODO can these be imported from another file?
 	let numLayers = 7;
@@ -45,7 +50,10 @@
 			storageKey.indexOf(mainCutType) + 2,
 			storageKey.indexOf("%")
 		); // 0-100
-		const numHorizontalCuts = storageKey[storageKey.indexOf("h") - 1]; // 0 | 1 | 2
+		const numHorizontalCuts = +storageKey[storageKey.indexOf("h") - 1]; // 0 | 1 | 2
+		const horizontalCutScale = scaleLinear()
+			.domain([0, numHorizontalCuts])
+			.range([1 / (numHorizontalCuts + 1), 1]);
 		const isRadial = mainCutType === "r";
 		const title = `${numCuts} ${mainCutTypeMap[mainCutType]} ${pluralize(
 			"cut",
@@ -54,17 +62,47 @@
 			isRadial ? `${cutTargetDepthPercentage}% depth, ` : ""
 		}${numHorizontalCuts} horizontal ${pluralize("cut", numHorizontalCuts)}`;
 
-		// TODO add dotted cut lines + depth labels
-		r.cuttingDiagram = `<svg viewBox="0 0 ${diagramSize} ${diagramSize}" width="${diagramSize}">
+		r.cuttingDiagram = `<svg viewBox="${-halfDiagramSize} ${-halfDiagramSize} ${diagramSize} ${diagramSize}" width="${diagramSize}">
 			<style>
 				rect {
 					fill: rgba(239, 239, 239, 1);
+				}
+
+				.cut {
+					stroke-dasharray: 2;
 				}
 			</style>
 
 			<title>${title}</title>
 
-			<rect width="${diagramSize}" height="${diagramSize}" />
+			<rect x="${-halfDiagramSize}" y="${-halfDiagramSize}" width="${diagramSize}" height="${diagramSize}" />
+
+			${
+				isRadial
+					? `
+							<!-- TODO is showing cutting board + vertical cut helpful? -->
+							<!-- <line x1="${-diagramExtent}" y1="${-diagramExtent}" x2="${diagramExtent}" y2="${-diagramExtent}" />
+							<line x1="0" y1="${-diagramExtent}" x2="0" y2="${radialTargetPosition}" class="cut" /> -->
+							<line x1="${diagramExtent}" y1="${-diagramExtent}" x2="0" y2="${radialTargetPosition}" class="cut" />
+
+							<circle r="1.5" cy="${radialTargetPosition}" />
+
+							<text y=${diagramExtent} text-anchor="middle" font-size="8">
+								${cutTargetDepthPercentage}%
+							</text>
+						`
+					: `<line x1="0" y1="${-diagramExtent}" x2="0" y2="${diagramExtent}" class="cut" />`
+			}
+
+			${
+				numHorizontalCuts
+					? Array.from({ length: numHorizontalCuts }).map((_, h) => {
+							const y =
+								-diagramExtent + horizontalCutScale(h) * 2 * diagramExtent;
+							return `<line x1="${-diagramExtent}" y1="${y}" x2="${diagramExtent}" y2="${y}" class="cut" />`;
+					  })
+					: ""
+			}
 		</svg>`;
 
 		return r;
--- src/utils/pluralize.js
+++ src/utils/pluralize.js
@@ -1,5 +1,8 @@
 const pr = new Intl.PluralRules("en-US");
-const suffixes = new Map([["one", "s"]]);
+const suffixes = new Map([
+	["one", ""],
+	["other", "s"]
+]);
 
 export function pluralize(noun, n) {
 	const rule = pr.select(n);
