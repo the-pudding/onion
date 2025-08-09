e4827b57507c303be5c4e698ecb38bddee347af4
Andrew Aquino <dawneraq@gmail.com>
2025-08-08 22:44:58 -0700

    feat: enable Letters with spaces to break lines

--- src/components/Letters.svelte
+++ src/components/Letters.svelte
@@ -1,7 +1,7 @@
 <script>
 	let { string, shrink, hed } = $props();
 
-	let letters = $derived(string.split(""));
+	let words = $derived(string.split(" "));
 
 	const rotations = [-4, -2, 0, 2, 4];
 
@@ -11,83 +11,70 @@
 </script>
 
 <div class="letters" aria-label={string} class:hed class:shrink>
-	{#each letters as letter, i}
-		{#if letter !== " "}
-			<img
-				src="assets/letters/{letter}.png"
-				alt="{letter} styled as an Onion"
-				style="transform: rotate({rotations[
-					getRandomIndex(rotations.length - 1)
-				]}deg);"
-			/>
-		{:else}
-			<span></span>
-		{/if}
+	{#each words as word}
+		<div class="word">
+			{#each word as letter}
+				<img
+					src="assets/letters/{letter}.png"
+					alt="{letter} styled as an Onion"
+					style="transform: rotate({rotations[
+						getRandomIndex(rotations.length - 1)
+					]}deg);"
+				/>
+			{/each}
+		</div>
 	{/each}
 </div>
 
 <style>
 	.letters {
 		display: flex;
-		flex-direction: row;
+		justify-content: center;
+		column-gap: 2rem;
+		flex-wrap: wrap;
+		margin-block: 2rem;
+	}
+
+	.hed {
+		margin-block: 0;
+
+		img {
+			height: 20vw;
+		}
+	}
+
+	.shrink {
+		margin-block: -1rem 2rem;
+	}
+
+	.word {
+		display: flex;
 		justify-content: center;
 		align-items: center;
 		gap: 0.5rem;
-		margin: 2rem auto;
-	}
-
-	.letters.hed {
-		margin: 0 auto;
 	}
 
 	img {
-		height: min(11vw);
+		height: 11vw;
 		min-height: 60px;
 		max-height: 200px;
 		filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.06))
 			drop-shadow(0 2px 2px rgba(0, 0, 0, 0.06))
 			drop-shadow(0 4px 4px rgba(0, 0, 0, 0.06));
 	}
-	.hed img {
-		height: min(20vw);
-	}
-
-	span {
-		display: inline-block;
-		width: 3vw;
-		min-width: 5px;
-		max-width: 150px;
-	}
-
-	.letters.shrink {
-		margin: -1rem auto;
-		margin-bottom: 2rem;
-	}
 
 	@media (min-width: 640px) {
-		.letters.hed,
-		.letters.shrink {
-			margin: -1rem auto;
-		}
+		.hed {
+			margin-block: -1rem;
 
-		.letters.shrink {
-			margin-bottom: 2rem;
-		}
-
-		span {
-			display: inline-block;
-			width: min(10vw, 20svh);
-			min-width: 25px;
-			max-width: 150px;
+			img {
+				height: min(30vw, 40svh);
+				max-height: 300px;
+			}
 		}
 
 		img {
 			height: min(14vw, 20svh);
-		}
-
-		.hed img {
-			height: min(30vw, 40svh);
-			max-height: 300px;
 		}
 	}
 </style>
